1b1c2e7c4fbfa224feb2d7028db3c09b16f96840
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-11-13 22:55:41 +0530

    feat(development): :construction: task file updated

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -5,3 +5,8 @@ export const signUp = async (req: Request, res: Response): Promise<Response> => 
   logger.debug("TODO: in progress", { req: req.body });
   return res.status(500).json({ todo: "todo" });
 };
+
+export const logIn = async (req: Request, res: Response): Promise<Response> => {
+  logger.debug("TODO: in progress", { req: req.body });
+  return res.status(500).json({ todo: "todo" });
+};
