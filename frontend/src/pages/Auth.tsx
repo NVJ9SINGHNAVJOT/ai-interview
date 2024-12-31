1b699acfee4c0d4a2322336a767ca297ea6e8ee6
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-12-31 22:51:17 +0530

    refactor(redis): :recycle: redis added and backend separated to server and db

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,5 +1,11 @@
+import { useParams } from "react-router-dom";
+
 const Auth = () => {
-  return <div>Auth</div>;
+  const { type } = useParams();
+
+  return <div className="w-full min-h-[calc(100%-3.8rem)] bg-neutral-800 flex justify-center items-center">
+
+  </div>;
 };
 
 export default Auth;
